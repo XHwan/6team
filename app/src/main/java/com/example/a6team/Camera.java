d660726f0f39bf5359d6cca60943d119114885d6
Seo <ws0051@nate.com>
2019-05-10 17:54:58 +0900

    yes

---
+++ app/src/main/java/com/example/a6team/Camera.java
@@ -0,0 +1,45 @@
+package com.example.a6team;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class Camera extends Activity
+{
+    ImageButton btn = null;
+    ImageView iv = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera);
+
+        setup();
+    }
+
+
+
+    private void setup()
+    {
+        btn = (ImageButton)findViewById(R.id.btn);
+        iv = (ImageView)findViewById(R.id.iv);
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
